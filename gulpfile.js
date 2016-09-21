f621ad6d6cd76344050222111eb153a3819eb7eb
Yilin Xu <yilin.xu@mastercard.com>
2016-09-21 13:33:56 -0400

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
 gulp.task('vet', function() {
+	log('Analyzing source with JSHint and JSCS');
 	return gulp.src([
 		'./src/**/*.js',
 		'./*.js'
 	])
+	.pipe(gulpif(args.verbose, gulpprint()))
 	.pipe(jscs())
 	.pipe(jshint())
-	.pipe(jshint.reporter('jshint-stylish', {verbose: true}));
-})
+	.pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+	.pipe(jshint.reporter('fail'));
+});
+
+///////////
+
+function log(msg) {
+	if (typeof(msg) === 'object') {
+		for (var item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				util.log(util.colors.blue(msg[item]));
+			}
+		}
+	} else {
+		util.log(util.colors.blue(msg));
+	}
+}
