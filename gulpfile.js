ead9554057ae0fd905ad210483bf161ef674b2be
Yilin Xu <yilin.xu@mastercard.com>
2016-09-21 12:44:42 -0400

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
-gulp.task('hello-world', function() {
-	console.log('Our first hello world gulp task!');
-});
+gulp.task('vet', function() {
+	return gulp.src([
+		'./src/**/*.js',
+		'./*.js'
+	])
+	.pipe(jscs())
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+})
