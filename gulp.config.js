c0cd500cbad72884e760180b43586db5770178fb
Yilin Xu <yilin.xu@mastercard.com>
2016-09-28 10:40:08 -0400

    wiredep

--- gulp.config.js
+++ gulp.config.js
@@ -9,14 +9,33 @@ module.exports = function() {
 			'./src/**/*.js',
 			'./*.js'
 		],
+		client: client,
 		index: client + 'index.html',
 		js: [
 			clientApp + '**/*.module.js',
 			clientApp + '**/*.js',
 			'!' + clientApp + '**/*.spec.js'
 		],
-		less: client + '/styles/styles.less'
-	};
+		less: client + '/styles/styles.less',
+		
+		/**
+		 * Bower and NPM locations
+		 */
+		bower: {
+			json: require('./bower.json'),
+			directory: './bower_components/',
+			ignorePath: '../..'
+		}
+	}
+	
+	config.getWiredepDefaultOptions = function() {
+		var options = {
+			bowerJson: config.bower.json,
+			directory: config.bower.directory,
+			ignorePath: config.bower.ignorePath
+		};
+		return options;
+	}
 	
 	return config;
 };
